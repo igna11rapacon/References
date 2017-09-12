b9cabab8896e56cc4b88e1802e6b50c79cda73af
Adrianne Tubig <adriannetubig@gmail.com>
2017-09-12 15:49:54 +0800

    Uploading Reference

---
+++ UploadingSample/UploadingSample/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadingSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        #region FormUpload
+        [HttpPost]
+        public ActionResult FormUpload(HttpPostedFileBase file)
+        {
+
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                fileName = fileName.Split('\\').Last(); //This will fix problems when uploading using IE
+                var path = Path.Combine(Server.MapPath("~/App_Data/FormFileUpload"), fileName);
+                file.SaveAs(path);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult FormMultipleUpload(IEnumerable<HttpPostedFileBase> files)
+        {
+            foreach (var file in files)
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    fileName = fileName.Split('\\').Last(); //This will fix problems when uploading using IE
+                    var path = Path.Combine(Server.MapPath("~/App_Data/FormFileUpload"), fileName);
+                    file.SaveAs(path);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        #endregion
+    }
+}
---
+++ UploadingSample/UploadingSample/Scripts/Angular/Controller/Upload.Controller.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('App')
+        .controller('UploadController', UploadController);
+
+    UploadController.$inject = ['UploadService'];
+
+    function UploadController(UploadService) {
+        var vm = this;
+
+        vm.Attachment;
+
+        vm.UploadAttachment = UploadAttachment;
+
+        function UploadAttachment() {
+            UploadService.Create(vm.Attachment)
+                .then(function (response) {
+                    alert('Success');
+                })
+                .catch(function (result) {
+                    alert('Fail');
+                    console.log(result);
+                });
+        }
+
+    }
+})();
